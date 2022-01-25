4ec53571a29015e63bd8c0506ae35f5947c74095
Leonardo Quintania <leonardoquintania@gmail.com>
2022-01-25 08:45:56 -0300

    Ajuste na configuração do portifolio

--- src/config.js
+++ src/config.js
@@ -2,10 +2,10 @@
 module.exports = {
   github: {
     username: 'leonardoquintania', // Your GitHub org/user name. (Required)
-    sortBy: 'stars', // stars | updated
-    limit: 8, // How many projects to display.
+    sortBy: 'updated', // stars | updated
+    limit: 6, // How many projects to display.
     exclude: {
-      forks: false, // Forked projects will not be displayed if set to true.
+      forks: True, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
@@ -94,15 +94,12 @@ module.exports = {
   },
   themeConfig: {
     default: 'halloween',
-
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
     disableSwitch: true,
-
     // Should we use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded default
     respectPrefersColorScheme: true,
-
     // Available themes. To remove any theme, exclude from here.
     themes: ['halloween'],
   },
